e4db26c56129e118a36fec6de6876fba1fca7580
Ahmad-byte-design <www.ahmadhusensy@gmail.com>
2026-09-10 13:09:11 +0300

    add url front to cors file of back end

--- server/config/cors.php
+++ server/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173'),"loom-mind-board.vercel.app"],
 
     'allowed_origins_patterns' => [],
 
